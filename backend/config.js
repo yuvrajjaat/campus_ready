d1e58c4dfc31938464a35ad70f8323a74576da05
Yuvraj Singh <130272717+yuvrajjaat@users.noreply.github.com>
2025-04-04 19:21:17 +0530

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB_URI: 'mongodb://localhost:27017/<your_database>', // adjust as needed
+    JWT_SECRET: 'jwt_secret', // use environment variables in production
+    UPLOAD_DIR: 'uploads/'
+  };
+  
